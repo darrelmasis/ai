83653b1f7c270a1ceae0f90cd19a052e00d6f362
Darrel Masis <darrelmasis@gmail.com>
2024-04-18 00:04:52 -0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,64 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'normalize.css';
+import { useState, useEffect } from 'react';
 import { Card, Container, Row, Col } from 'react-bootstrap';
+import data from './assets/extras.json'
 
 function App() {
+  const [employees, setEmployees] = useState([]); // Inicializa el estado como un array vacío
+  const limit = 18
 
-  const empleados = [
-    {
-      nombre: 'Darel',
-      horasExtraDisponibles: 10,
-      fotoURL: 'https://example.com/juan.jpg'
-    },
-    {
-      nombre: 'Kailuma',
-      horasExtraDisponibles: 5,
-      fotoURL: 'https://example.com/maria.jpg'
-    },
-    {
-      nombre: 'Bryan',
-      horasExtraDisponibles: 8,
-      fotoURL: 'https://example.com/pedro.jpg'
-    },
-    {
-      nombre: 'Yeser',
-      horasExtraDisponibles: 12,
-      fotoURL: 'https://example.com/laura.jpg'
-    },
-    {
-      nombre: 'Anthony',
-      horasExtraDisponibles: 3,
-      fotoURL: 'https://example.com/carlos.jpg'
-    }
-  ];
+  useEffect(() => {
+    // Ordenar de manera descendente el objeto
+    const sortedData = data.sort((a, b) => a.currentTime - b.currentTime);
+    setEmployees(sortedData);
+  }, []);
+
+  const avatar = (str) => {
+    const settings = {
+      size: 128,
+      'font-size': 0.5,
+      length: 2,
+      name: str,
+      rounded: true,
+      bold: false,
+      background: 'random',
+      color: '8b5d5d',
+      uppercase: false,
+      format: 'svg'
+    };
+
+    const queryString = Object.keys(settings)
+      .map(key => `${key}=${encodeURIComponent(settings[key])}`)
+      .join('&');
+
+    const url = `https://ui-avatars.com/api/?${queryString}`;
+
+    return url;
+  }
 
   return (
     <>
-      <Container className='mt-3'>
-        <div className="content">
-          <Row xs={1} sm={2} md={3} lg={4} xl={5} className='d-flex'>
-            {empleados.map((empleado, index) => (
+      <Container>
+          <Row xs={2} sm={2} md={3} lg={4} xl={5} className='d-flex g-4 g-lg-3 content'>
+            {employees.map((employee, index) => (
               <Col key={index} className=''>
-                <Card className='text-center'>
-                  <Card.Img variant='top' src={empleado.fotoURL} />
+                <Card className='text-center p-3'>
+                  <Card.Img variant='top' className='avatar' src={avatar(employee.name)} />
                   <Card.Body>
-                    <Card.Title>{empleado.nombre}</Card.Title>
+                    <Card.Title className='fw-light'>{employee.name.split(' ')[0]}</Card.Title>
                     <Card.Text>
-                      Horas extra disponibles:
-                      <h3>{empleado.horasExtraDisponibles}</h3>
+                      Disponible:
+                      <span className='d-block mt-3 fw-light fs-1'>{limit - employee.currentTime}</span>
+                    </Card.Text>
+                    <Card.Text>
+                      <span className='d-block mt-3 fw-light'>H/E:  {employee.currentTime}</span>
                     </Card.Text>
                   </Card.Body>
                 </Card>
               </Col>
             ))}
           </Row>
-        </div>
       </Container>
     </>
   );
